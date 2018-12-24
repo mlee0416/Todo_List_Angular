1da4642ad0fd10afb8df3162dd34542b672b04cd
YAPI <yapi@YAPIs-iMac-4.localdomain>
2018-12-24 09:04:09 -0800

    alpha

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  todoArray = [];
+
+
+   addTodo(value){
+       this.todoArray.push(value)
+       console.log(this.todos)
+     }
+
+
+  /*delete item*/
+  deleteItem(todo){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+     if(todo== this.todoArray[i]){
+      this.todoArray.splice(i,1)
+     }
+    }
+   }
+}
